1b5713e97ce7844a56601a9251c05cdae35c27bf
111vandit111 <vanditkala@gmail.com>
2025-04-07 21:24:51 +0530

    Chores: added SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,12 +13,55 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Vandit Kala | Developer & AI Enthusiast",
+  title: "Vandit Kala | Frontend Developer & AI Enthusiast",
   description:
-    "I’m Vandit Kala, a passionate Frontend & Developer with expertise in React.js, Next.js, TypeScript, and MongoDB. I build scalable, high-performance web applications and have experience optimizing React apps, creating custom hooks, and reducing bundle sizes. I’ve worked on AI-driven platforms like Hoonr.ai and UXMagic.ai, integrating AI for automation and efficiency. Let’s connect and build something amazing!",
-  icons:{
-    icon:"/log-w.png"
-  }
+    "Vandit Kala is a skilled Frontend Developer with deep expertise in React.js, Next.js, TypeScript, and MongoDB. Specializing in scalable web apps and AI integrations for platforms like Hoonr.ai and UXMagic.ai. Let's build something extraordinary.",
+  keywords: [
+    "Vandit Kala",
+    "Frontend Developer",
+    "Next.js Developer",
+    "React.js Developer",
+    "TypeScript Developer",
+    "MongoDB",
+    "AI Developer",
+    "Hoonr.ai",
+    "UXMagic.ai",
+    "Full-Stack Developer",
+    "Software Engineer",
+    "Web Development",
+    "Generative AI",
+    "Custom Hooks",
+    "Bundle Optimization",
+  ],
+  authors: [{ name: "Vandit Kala", url: "https://vandit-kala.vercel.app" }],
+  creator: "Vandit Kala",
+  openGraph: {
+    title: "Vandit Kala | Frontend Developer & AI Enthusiast",
+    description:
+      "Explore the portfolio of Vandit Kala, a developer building high-performance React/Next.js apps and integrating AI to solve complex problems.",
+    url: "https://vandit-kala.vercel.app",
+    siteName: "Vandit Kala",
+    images: [
+      {
+        url: "/log-w.png",
+        width: 1200,
+        height: 630,
+        alt: "Vandit Kala Portfolio Preview",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vandit Kala | Frontend Developer & AI Enthusiast",
+    description:
+      "Developer with a passion for crafting fast, scalable web apps and leveraging AI for efficiency and automation.",
+    images: ["/log-w.png"],
+    creator: "@111vandit111 Github", 
+  },
+  icons: {
+    icon: "/log-w.png",
+  },
 };
 
 export default function RootLayout({
@@ -29,7 +72,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-secondary-900  antialiased text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-secondary-900 antialiased text-white`}
       >
         {children}
       </body>
